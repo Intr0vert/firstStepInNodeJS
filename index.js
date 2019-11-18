f5eed976f27e9f584a435c846f4cea0f72088c2a
Intr0vert <Intr0vert.noname@mail.ru>
2019-11-18 12:41:53 +0300

    first step in NodeJS

---
+++ index.js
@@ -0,0 +1,27 @@
+let http = require("http");
+let fs = require("fs");
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	if (req.url == "/") {
+		fs.readFile("index.html", function(err, data) {
+			res.write(data);
+			res.end();
+		});
+	} else if (req.url == "/contact-me") {
+		fs.readFile("contact-me.html", function(err, data) {
+			res.write(data);
+			res.end();
+		});
+	} else if (req.url == "/about") {
+		fs.readFile("about.html", function(err, data) {
+			res.write(data);
+			res.end();
+		});
+	} else {
+		fs.readFile("404.html", function(err, data) {
+			res.write(data);
+			res.end();
+		});
+	}
+}).listen(8080);
